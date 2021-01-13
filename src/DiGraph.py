5bdb7845cf58bdb4bdc36aee0e63c8c5e82e9699
Hadas-l <73165756+Hadas-l@users.noreply.github.com>
2021-01-13 20:00:12 +0200

    Add files via upload

---
+++ src/DiGraph.py
@@ -0,0 +1,211 @@
+import copy
+
+
+class DiGraph:
+    """
+    This class represents a bi-directional graph
+    with positive weights.
+
+    Graph functionality as follows:
+    -- add node
+    -- add edge
+    -- get number of nodes in the graph
+    -- get number of edges in the graph
+    -- get number of operations done on the graph
+    -- get neighbors of a node
+    -- remove node
+    -- remove edge
+    """
+
+    class DiNode:
+        """
+        A simple class to represent a Node
+        in a graph with simple data : key, position
+        """
+        def __init__(self, key, pos=(0, 0, 0)):
+            self.id = key
+            self.pos = pos
+
+    def __init__(self, graph=None):
+        self.nodes = {}
+        self.neighbors = {}
+        self.connected_to = {}
+
+        self.mc = 0
+        self.edge_size = 0
+        self.node_size = 0
+
+        if graph:
+            self.nodes = copy.deepcopy(graph.nodes)
+            self.neighbors = copy.deepcopy(graph.neighbors)
+            self.connected_to = copy.deepcopy(graph.connected_to)
+
+            self.mc = graph.mc
+            self.edge_size = graph.edge_size
+            self.node_size = graph.node_size
+
+    def v_size(self) -> int:
+        """
+        Returns the number of vertices in this graph
+        @return: The number of vertices in this graph
+        """
+        return self.node_size
+
+    def e_size(self) -> int:
+        """
+        Returns the number of edges in this graph
+        @return: The number of edges in this graph
+        """
+        return self.edge_size
+
+    def get_all_v(self) -> dict:
+        """return a dictionary of all the nodes in the Graph, each node is represented using apair  (key, node_data)
+        """
+        return self.nodes
+
+    def all_in_edges_of_node(self, id1: int) -> dict:
+        """return a dictionary of all the nodes connected to (into) node_id ,
+        each node is represented using a pair (key, weight)
+         """
+
+        return self.connected_to[id1] if id1 in self.nodes else {}
+
+    def all_out_edges_of_node(self, id1: int) -> dict:
+        """return a dictionary of all the nodes connected from node_id , each node is represented using a pair (key,
+        weight)
+        """
+
+        return self.neighbors[id1] if id1 in self.nodes else {}
+
+    def get_mc(self) -> int:
+        """
+        Returns the current version of this graph,
+        on every change in the graph state - the MC should be increased
+        @return: The current version of this graph.
+        """
+        return self.mc
+
+    def add_edge(self, id1: int, id2: int, weight: float) -> bool:
+        """
+        Adds an edge to the graph.
+        @param id1: The start node of the edge
+        @param id2: The end node of the edge
+        @param weight: The weight of the edge
+        @return: True if the edge was added successfully, False o.w.
+        Note: If the edge already exists or one of the nodes dose not exists the functions will do nothing
+        """
+
+        if all(key in self.nodes for key in (id1, id2)):
+
+            if weight >= 0:
+
+                if id2 not in self.neighbors[id1] and id1 not in self.connected_to[id2]:
+
+                    self.neighbors[id1][id2] = weight
+                    self.connected_to[id2][id1] = weight
+
+                    self.edge_size += 1
+                    self.mc += 1
+
+                    return True
+
+        return False
+
+    def add_node(self, node_id: int, pos: tuple = None) -> bool:
+        """
+        Adds a node to the graph.
+        @param node_id: The node ID
+        @param pos: The position of the node
+        @return: True if the node was added successfully, False o.w.
+        Note: if the node id already exists the node will not be added
+        """
+
+        if node_id not in self.nodes:
+
+            if pos:
+                self.nodes[node_id] = DiGraph.DiNode(node_id, pos)
+            else:
+                self.nodes[node_id] = DiGraph.DiNode(node_id)
+
+            self.neighbors[node_id] = {}
+            self.connected_to[node_id] = {}
+
+            self.node_size += 1
+            self.mc += 1
+            return True
+
+        return False
+
+    def remove_node(self, node_id: int) -> bool:
+        """
+        Removes a node from the graph.
+        @param node_id: The node ID
+        @return: True if the node was removed successfully, False o.w.
+        Note: if the node id does not exists the function will do nothing
+        """
+
+        if node_id in self.nodes:
+
+            for n in self.neighbors[node_id]:
+                del self.connected_to[n][node_id]
+                self.edge_size -= 1
+                self.mc += 1
+
+            for n in self.connected_to[node_id]:
+                del self.neighbors[n][node_id]
+                self.edge_size -= 1
+                self.mc += 1
+
+            self.node_size -= 1
+            self.mc += 1
+            del self.nodes[node_id]
+
+            return True
+
+        return False
+
+    def remove_edge(self, node_id1: int, node_id2: int) -> bool:
+        """
+        Removes an edge from the graph.
+        @param node_id1: The start node of the edge
+        @param node_id2: The end node of the edge
+        @return: True if the edge was removed successfully, False o.w.
+        Note: If such an edge does not exists the function will do nothing
+        """
+
+        if all(key in self.nodes for key in (node_id1, node_id2)):
+
+            if node_id2 in self.neighbors[node_id1] and node_id1 in self.connected_to[node_id2]:
+
+                del self.neighbors[node_id1][node_id2]
+                del self.connected_to[node_id2][node_id1]
+
+                self.edge_size -= 1
+                self.mc += 1
+
+                return True
+
+        return False
+
+    def transpose(self):
+        """
+        transpose graph
+        :return: transposed graph
+        """
+
+        transposed = DiGraph()
+
+        for node in self.get_all_v():
+            transposed.add_node(node)
+
+        for node in self.get_all_v():
+            for ni in self.all_out_edges_of_node(node):
+                transposed.add_edge(ni, node, self.neighbors[node][ni])
+
+        return transposed
+
+    def __repr__(self):
+        return "|V|={} , |E|={}".format(self.v_size(), self.e_size())
+
+    def __str__(self):
+        return "|V|={} , |E|={}".format(self.v_size(), self.e_size())
---
+++ src/GraphAlgo.py
@@ -0,0 +1,301 @@
+from typing import List
+from src import GraphInterface
+import json
+import sys
+from src.DiGraph import DiGraph
+import heapq
+import matplotlib.pyplot as plt
+
+
+class GraphAlgo:
+
+    def __init__(self, graph=None):
+        self.graph = graph
+
+    def get_graph(self) -> GraphInterface:
+        """
+        :return: the directed graph on which the algorithm works on.
+        """
+        return self.graph
+
+    def load_from_json(self, file_name: str) -> bool:
+        """
+        Loads a graph from a json file.
+        @param file_name: The path to the json file
+        @returns True if the loading was successful, False o.w.
+        """
+
+        try:
+
+            with open(file_name, 'r') as file:
+
+                graph_dict = json.load(file)
+
+                graph = DiGraph()
+
+                for node_json in graph_dict['Nodes']:
+
+                    try:
+                        split_pos = node_json['pos'].split(",")
+                        x, y, z = float(split_pos[0]), float(split_pos[1]), float(split_pos[2])
+                        graph.add_node(node_json['id'], (x, y, z))
+
+                    except:
+                        graph.add_node(node_json['id'])
+
+                for edge_json in graph_dict["Edges"]:
+                    graph.add_edge(edge_json['src'], edge_json['dest'], edge_json['w'])
+
+                self.graph = DiGraph(graph)
+                return True
+
+        except OSError as err:
+            print("error: {}".format(err))
+            return False
+
+        except:
+            print("unexpected error ", sys.exc_info()[0])
+            return False
+
+    def save_to_json(self, file_name: str) -> bool:  # {
+        """
+        Saves the graph in JSON format to a file
+        @param file_name: The path to the out file
+        @return: True if the save was successful, Flase o.w.
+        """
+        try:
+
+            graph_json = {"Nodes": [], "Edges": []}
+
+            for node_key in self.graph.get_all_v():
+                node = self.graph.get_all_v()[node_key]
+
+                # convert pos from tuple to string
+                x, y, z = node.pos
+                pos_string = "{},{},{}".format(x, y, z)
+
+                graph_json["Nodes"].append({
+                    "id": node_key,
+                    "pos": pos_string
+                })
+
+                for out in self.graph.all_out_edges_of_node(node_key):
+
+                    graph_json["Edges"].append({
+                        "src": node_key,
+                        "dest": out,
+                        "w": self.graph.all_out_edges_of_node(node_key)[out]
+                    })
+
+            with open(file_name, 'w') as file:
+                json.dump(graph_json, file)
+
+                return True
+
+        except OSError as err:
+            print("error: {}".format(err))
+            return False
+
+        except:
+            print("unexpected error ", sys.exc_info()[0])
+            return False
+
+    def djikstra(self, src_node: int) -> (list, list):
+        """
+        Djikstra's algorithm with priority queue
+
+        More info:
+        https://en.wikipedia.org/wiki/Dijkstra's_algorithm
+        :param src_node: starting node
+        :return:
+        """
+
+        distance = {i: float('inf') for i in self.graph.get_all_v()}
+        parents = {i: None for i in self.graph.get_all_v()}
+
+        distance[src_node] = 0.0
+        priority_queue = [(distance[src_node], src_node)]
+
+        visited = set()
+
+        while priority_queue:
+
+            _, key = heapq.heappop(priority_queue)
+
+            if key not in visited:
+                visited.add(key)
+
+                for ni_key in self.graph.all_out_edges_of_node(key):
+
+                    if ni_key not in visited:
+                        # visited.add(ni_key)
+
+                        alt = _ + self.graph.all_out_edges_of_node(key)[ni_key]
+
+                        if alt < distance[ni_key]:
+                            distance[ni_key] = alt
+                            parents[ni_key] = key
+
+                            heapq.heappush(priority_queue, (alt, ni_key))
+
+        return distance, parents
+
+    def shortest_path(self, id1: int, id2: int) -> (float, list):
+        """
+        Returns the shortest path from node id1 to node id2 using Dijkstra's Algorithm
+        @param id1: The start node id
+        @param id2: The end node id
+        @return: The distance of the path, the path as a list
+        Example:
+#      >>> from GraphAlgo import GraphAlgo
+#       >>> g_algo = GraphAlgo()
+#        >>> g_algo.addNode(0)
+#        >>> g_algo.addNode(1)
+#        >>> g_algo.addNode(2)
+#        >>> g_algo.addEdge(0,1,1)
+#        >>> g_algo.addEdge(1,2,4)
+#        >>> g_algo.shortestPath(0,1)
+#        (1, [0, 1])
+#        >>> g_algo.shortestPath(0,2)
+#        (5, [0, 1, 2])
+        More info:
+        https://en.wikipedia.org/wiki/Dijkstra's_algorithm
+        """
+        if id1 in self.graph.get_all_v() and id2 in self.graph.get_all_v() and id1 is not id2:
+
+            distance, parents = self.djikstra(id1)
+            shortest_dist = distance[id2]
+
+            parent_key = id2
+            shortest_path = [parent_key]
+
+            while parent_key is not None:
+
+                shortest_path.append(parents[parent_key])
+                parent_key = parents[parent_key]
+
+                if parent_key == id1:
+                    return shortest_dist, shortest_path[::-1]
+
+                if parent_key is None:
+                    return shortest_dist, []
+
+        return float('inf'), []
+
+    def dfs(self, v: int, visited: dict, final, graph=None) -> list:
+        """
+        utill function to scan graph recursively with DFS
+        :param graph: transposed graph - default to none
+        :param final: strongly connected component list
+        :param nodes: dict of nodes for the current graph dfs will run on
+        :param v: current node
+        :param visited: list of visited vertexes
+        :return: list representing a strongly connected component
+        """
+        if graph:
+            visited[v] = True
+
+            final.append(v)
+
+            for out in graph.all_out_edges_of_node(v):
+                if not visited[out]:
+                    self.dfs(out, visited, final, graph)
+
+            return final
+
+        else:
+            visited[v] = True
+
+            outs = self.graph.all_out_edges_of_node(v)
+            for out in outs:
+                if not visited[out]:
+                    self.dfs(self.graph.get_all_v()[out].id, visited, final)
+            final.append(v)
+
+    def connected_component(self, id1: int) -> list:
+        """
+        Finds the Strongly Connected Component(SCC) that node id1 is a part of.
+        @param id1: The node id
+        @return: The list of nodes in the SCC
+        """
+
+        for i in self.connected_components():
+            if id1 in i:
+                return i
+
+        return []
+
+    def connected_components(self) -> List[list]:
+        """
+        Finds all the Strongly Connected Component(SCC) in the graph.
+        @return: The list all SCC
+        """
+        stack = []
+
+        visited = {i: False for i in self.graph.get_all_v()}
+
+        for v in self.graph.get_all_v():
+            if not visited[v]:
+                self.dfs(v, visited, stack)
+
+        transposed = self.get_graph().transpose()
+
+        visited = {i: False for i in self.graph.get_all_v()}
+
+        res = []
+
+        while stack:
+            key = stack.pop()
+            if not visited[key]:
+                component = self.dfs(key, visited, [], graph=transposed)
+                res.append(component)
+
+        return res
+
+    def plot_graph(self) -> None:
+        """
+        Plots the graph.
+        If the nodes have a position, the nodes will be placed there.
+        Otherwise, they will be placed in a random but elegant manner.
+        @return: None
+        """
+
+        nodes = self.graph.get_all_v()
+
+        x_, y_, z_ = [], [], []
+        xy_id = []
+        ax = plt.axes()
+        for n in nodes:
+            node_ = nodes[n]
+            if node_.pos == (0, 0, 0):
+                x, y, z = node_.id, node_.id, 0
+            else:
+                (x, y, z) = node_.pos
+
+            for out in self.graph.all_out_edges_of_node(n):
+
+                o_ = nodes[out]
+
+                if o_.pos == (0, 0, 0):
+                    o_x, o_y, o_z = o_.id, o_.id, 0
+                else:
+                    (o_x, o_y, o_z) = o_.pos
+
+                # plot arrows between nodes
+                ax.quiver(x, y, o_x-x, o_y-y, angles='xy', scale_units='xy', scale=1, width=0.005)
+
+            xy_id.append((x, y, node_.id))
+            x_.append(x)
+            y_.append(y)
+            z_.append(z)
+
+        # plt.xlim(left=min(x_), right=max(x_))
+        # plt.ylim(bottom=min(y_), top=max(y_))
+
+        # plot node id's
+        for i, (x, y, id_) in enumerate(xy_id):
+            plt.annotate(id_, (x, y), fontsize='xx-large', color='red')
+
+        # plot nodes
+        plt.plot(x_, y_, 'bo')
+        plt.show()
